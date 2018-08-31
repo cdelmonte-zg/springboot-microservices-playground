3fa824babd29f4e738c4dcddb9e128f4677da51d
Christian Del Monte <c.monte@qipu.de>
2018-08-31 12:42:03 +0200

    second commit

--- runner.sh
+++ runner.sh
@@ -3,5 +3,5 @@
 docker rm -fv $(docker ps -qa)
 docker volume rm $(docker volume ls -qf dangling=true)
 export BUILD_NAME="testing"
-mvn clean package docker:build
+mvn clean generate-sources package docker:build
 docker-compose -f docker-infrastructure/infra-dev.yml up --remove-orphans
