befe3cc38c6649c51ae39af51a6fd96e831253d9
Christian Del Monte <c.monte@qipu.de>
2018-09-20 15:24:50 +0200

    disable csrf on config endpoints

--- configuration-server/src/main/java/de/cdelmonte/confsvr/config/WebSecurityConfig.java
+++ configuration-server/src/main/java/de/cdelmonte/confsvr/config/WebSecurityConfig.java
@@ -5,12 +5,11 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-
 @EnableWebSecurity
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-  @Override
-  protected void configure(HttpSecurity http) throws Exception {
-    http.authorizeRequests().anyRequest().anonymous();
-  }
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable().authorizeRequests().anyRequest().anonymous();
+	}
 }
